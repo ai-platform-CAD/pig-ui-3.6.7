a8616da9a2e18728538741c2b2567791093c2758
Travis <46567145+TravisRoad@users.noreply.github.com>
2023-05-10 10:55:17 +0800

    add demo code

---
+++ src/views/demo/gendatasourceconf/index.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="mod-config">
+    <basic-container>xxx</basic-container>
+  </div>
+</template>
+
+<script>
+import { fetchList, delObj } from "@/api/gendatasourceconf";
+import TableForm from "./gendatasourceconf-form.vue";
+import { mapGetters } from "vuex";
+export default {
+  data() {
+    return {
+      dataForm: {
+        key: "",
+      },
+      dataList: [],
+      pageIndex: 1,
+      pageSize: 10,
+      totalPage: 0,
+      dataListLoading: false,
+      addOrUpdateVisible: false,
+    };
+  },
+  components: {
+    TableForm,
+  },
+  created() {
+    // this.getDataList();
+  },
+  computed: {
+    ...mapGetters(["permissions"]),
+  },
+  methods: {
+    // 获取数据列表
+    getDataList() {
+      this.dataListLoading = true;
+      fetchList(
+        Object.assign({
+          current: this.pageIndex,
+          size: this.pageSize,
+        })
+      ).then((response) => {
+        this.dataList = response.data.data.records;
+        this.totalPage = response.data.data.total;
+      });
+      this.dataListLoading = false;
+    },
+    // 每页数
+    sizeChangeHandle(val) {
+      this.pageSize = val;
+      this.pageIndex = 1;
+      this.getDataList();
+    },
+    // 当前页
+    currentChangeHandle(val) {
+      this.pageIndex = val;
+      this.getDataList();
+    },
+    // 新增 / 修改
+    addOrUpdateHandle(id) {
+      this.addOrUpdateVisible = true;
+      this.$nextTick(() => {
+        this.$refs.addOrUpdate.init(id);
+      });
+    },
+    // 删除
+    deleteHandle(id) {
+      this.$confirm("是否确认删除ID为" + id, "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning",
+      })
+        .then(function () {
+          return delObj(id);
+        })
+        .then((data) => {
+          this.$message.success("删除成功");
+          this.getDataList();
+        })
+        .catch(() => {});
+    },
+  },
+};
+</script>
