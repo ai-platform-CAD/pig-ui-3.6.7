cfe45101311386bf14fb3a99f2c05f705e67cabf
shijiahao314 <shijiahao314@foxmail.com>
2023-05-10 10:29:58 +0800

    调整样式

--- src/api/operator.js
+++ src/api/operator.js
@@ -20,39 +20,39 @@ import request from '@/router/axios'
 let baseurl = '/operator/manage';
 
 export function fetchList(query) {
-  return request({
-    url: baseurl + '/page',
-    method: 'get',
-    params: query
-  })
+    return request({
+        url: baseurl + '/page',
+        method: 'get',
+        params: query
+    })
 }
 
 export function addObj(obj) {
-  return request({
-    url: baseurl,
-    method: 'post',
-    data: obj
-  })
+    return request({
+        url: baseurl,
+        method: 'post',
+        data: obj
+    })
 }
 
 export function getObj(id) {
-  return request({
-    url: baseurl + '/' + id,
-    method: 'get'
-  })
+    return request({
+        url: baseurl + '/' + id,
+        method: 'get'
+    })
 }
 
 export function delObj(id) {
-  return request({
-    url: baseurl + '/' + id,
-    method: 'delete'
-  })
+    return request({
+        url: baseurl + '/' + id,
+        method: 'delete'
+    })
 }
 
 export function putObj(obj) {
-  return request({
-    url: baseurl,
-    method: 'put',
-    data: obj
-  })
+    return request({
+        url: baseurl,
+        method: 'put',
+        data: obj
+    })
 }
