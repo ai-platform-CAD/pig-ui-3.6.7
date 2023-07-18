a906338f1b0ebac57e257d5cd169556d0d81781f
ZHY <845761590@qq.com>
2023-07-18 17:20:08 +0800

    提交

--- src/views/foo/index.vue
+++ src/views/foo/index.vue
@@ -2,39 +2,37 @@
   <div class="common-layout">
     <el-card class="box-card">
       <template #header>
-        <div class="card-header">
-          <el-row :gutter="12">
-            <el-col :span="8">
-              <el-card shadow="hover">project</el-card>
-            </el-col>
-            <el-col :span="8">
-                <el-cascader v-model="value" :options="options" @change="handleChange" />
-            </el-col>
-            <el-col :span="8">
-              <el-card>
-                <el-button type="primary" class="first_buttun" @click="pageSwap">在线编辑</el-button>
-              </el-card>
-            </el-col>
-          </el-row>
+        <div class="row-container">
+          <el-card shadow="hover">project</el-card>
+          <el-card style="margin-left: auto;"><el-cascader v-model="value" :options="options" @change="handleChange" placeholder="实例状态" /></el-card>
+          <el-button type="primary" class="first_buttun" @click="pageSwap">在线编辑</el-button>
         </div>
       </template>
 
       <el-descriptions title="设备详细信息" :column="1" direction="horizontal" border>
         <el-descriptions-item
-            label="实例状态"
-            label-align="left"
-            align="center"
-            label-class-name="my-label"
-            class-name="my-content"
-            width="5px"
-        >working</el-descriptions-item
-        >
-        <el-descriptions-item label="深度学习框架" label-align="left" align="center"
-        >eslinst</el-descriptions-item
-        >
-        <el-descriptions-item label="关联数据集" label-align="left" align="center"
-        >L1</el-descriptions-item
-        >
+            label="实例状态"  label-align="left" align="center" label-class-name="my-label" class-name="my-content" width="5px">
+            working
+            </el-descriptions-item>
+        <el-descriptions-item label="深度学习框架" label-align="left" align="center">eslinst</el-descriptions-item>
+        <el-descriptions-item label="关联数据集" label-align="left" align="center">L1</el-descriptions-item>
+        <el-descriptions-item label="实例描述" label-align="left" align="center">
+          <el-tag size="small">test</el-tag>
+        </el-descriptions-item>
+        <el-descriptions-item label="编码资源" label-align="left" align="center">123Province</el-descriptions-item>
+        <el-descriptions-item label="训练资源" label-align="left" align="center">1</el-descriptions-item>
+        <el-descriptions-item label="创建时间" label-align="left" align="center">{{currentTime}}</el-descriptions-item>
+        <el-descriptions-item label="更新时间" label-align="left" align="center">{{currentTime}}</el-descriptions-item>
+      </el-descriptions>
+
+      <div class="divide-container"></div>
+      <el-descriptions title="训练套件" :column="1" direction="horizontal" border>
+        <el-descriptions-item
+            label="实例状态"  label-align="left" align="center" label-class-name="my-label" class-name="my-content" width="5px">
+            working
+            </el-descriptions-item>
+        <el-descriptions-item label="深度学习框架" label-align="left" align="center">eslinst</el-descriptions-item>
+        <el-descriptions-item label="关联数据集" label-align="left" align="center">L1</el-descriptions-item>
         <el-descriptions-item label="实例描述" label-align="left" align="center">
           <el-tag size="small">test</el-tag>
         </el-descriptions-item>
@@ -44,12 +42,13 @@
         <el-descriptions-item label="更新时间" label-align="left" align="center">{{currentTime}}</el-descriptions-item>
       </el-descriptions>
     </el-card>
-
+    
   </div>
 </template>
 
 <script lang="ts">
 import {defineComponent, ref} from 'vue'
+import { ElNotification } from 'element-plus'
 export default defineComponent({
   name: "index",
   components:{
@@ -60,26 +59,33 @@ export default defineComponent({
     // 多选状态提交
     const options = [
       {
-        value: '状态1',
-        label: '状态1',
-        children: [
-          {
-            value: '状态1-1',
-            label: '状态1-1',
-          },
-        ],
+        value: '创建',
+        label: '创建',
       },
       {
-        value: '状态2',
-        label: '状态2',
+        value: '启动',
+        label: '启动',
       },
       {
-        value: '状态3',
-        label: '状态3',
+        value: '停止',
+        label: '停止',
+      },
+      {
+        value: '重建',
+        label: '重建',
       },
     ]
 
+    const showPopup = ref(false);
     const handleChange = (value) => {
+      showPopup.value = true
+      // 可以从后端获取相关数据来进行判断是否成功
+      ElNotification({
+        title: 'Success',
+        message: '执行成功',
+        type: 'success',
+        duration: 2000,
+      })
       console.log(value)
     }
 
@@ -99,6 +105,7 @@ export default defineComponent({
       handleChange,
       pageSwap,
       currentTime,
+      showPopup,
     }
   }
 
@@ -106,28 +113,10 @@ export default defineComponent({
 </script>
 
 <style scoped>
-.el-row {
-  margin-bottom: 20px;
-}
-.el-row:last-child {
-  margin-bottom: 0;
-}
-.el-col {
-  border-radius: 4px;
-}
-
-.grid-content {
-  border-radius: 4px;
-  min-height: 36px;
-}
-
-.radius {
-  height: 50px;
-  width: 12%;
-  border:solid 2px gray;
-  border-radius: 10px;
-  margin-top: 20px;
-  margin-left: 30px;
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
 .first_buttun {
@@ -140,48 +129,22 @@ export default defineComponent({
   align-items: center;
 }
 
-.text {
-  font-size: 14px;
-}
-
-.item {
-  margin-bottom: 18px;
-}
-
 .box-card {
   margin-top: 2%;
   margin-left: 1%;
   width: 70%;
   height: 60%;
 }
-
-.container {
+.divide-container{
+  margin-bottom: 20px;
+}
+.row-container {
   display: flex;
-  margin: 20px;
-  height: 500px;
+  justify-content: flex-start;
 }
-
-.nav {
-  flex: 1;
-}
-
-.nav ul {
-  list-style: none;
-  padding: 0;
-}
-
-.nav li {
-  margin-bottom: 10px;
-}
-
-.nav a {
-  text-decoration: none;
-}
-
-.my-label {
-  background: var(--el-color-success-light-9);
-}
-.my-content {
-  background: var(--el-color-danger-light-9);
+.row-container2 {
+  display: flex;
+  justify-content:  flex-end;
 }
 </style>
+
